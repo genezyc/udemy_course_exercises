4dc16a84c76831e734a7fa3f87e0274abe7b584c
Gene 涌川 Zhang <genezhang@gmail.com>
2018-10-12 21:16:07 -0700

    react/random color boxes - first draft

--- react/random-boxes-starter/src/App.js
+++ react/random-boxes-starter/src/App.js
@@ -3,19 +3,51 @@ import './App.css';
 
 class App extends Component {
   
+  static randomColor() {
+    let randIndex = Math.floor( Math.random() * App.defaultProps.allColors.length );
+    return App.defaultProps.allColors[ randIndex ];
+  } 
   constructor(props) {
     super(props);
+    //set initial random colors
+    this.state = {
+      colors: (new Array( App.NUM_BOXES )).fill().map( ( _ ) => App.randomColor() ),
+    };
+
+    setInterval( (e) => {
+      let new_colors = Array.from( this.state.colors );
+      let randIndex = Math.floor( Math.random() * new_colors.length );
+      new_colors[ randIndex ] = App.randomColor();
+      this.setState( { colors: new_colors });
+    }, 300 );
+
   }
   
   render() {
+    // generate boxes
+    const boxes = this.state.colors.map( ( color, i ) => (
+        <Box index={i} color={ color }/>
+    ));
 
     return (
-      <div className="App">
-        Render boxes here
+      <div className="App" style={{
+        display: "flex",
+        flexWrap: "wrap", // so boxes flow over several pages
+      }}>
+        {boxes}
       </div>
     );
   }
-}
+};
+App.NUM_BOXES = 32;
+
+const Box = ( prop ) => (
+  <div style={{
+    backgroundColor: prop.color,
+    minWidth: "calc( 100vw / 8 )",  // 8 per row
+    height: "calc( 100vw / 8 )",
+  }}></div>
+);
 
 App.defaultProps = {
   allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
